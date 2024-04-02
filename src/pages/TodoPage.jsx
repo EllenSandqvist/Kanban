4cbeecb59ff6fb2b5b7dbf44a9a97979d2047e9f
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-04-02 14:11:21 +0200

    added a link in form of a closing sign to get the user back to home

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,12 +1,17 @@
 import { useContext } from "react";
 import TaskContext from "../context/TaskContext";
 import Column from "../components/Column";
+import { IoClose } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const TodoPage = () => {
   const { todoTasks } = useContext(TaskContext);
   return (
     <main>
       <Column columnTitle="Todo" tasks={todoTasks} />
+      <Link className="close-link" to="/">
+        <IoClose />
+      </Link>
     </main>
   );
 };
