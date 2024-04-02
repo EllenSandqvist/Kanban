37ddd8051d88ab9dc8ffb8c019ebbdbeb571406d
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-04-02 14:17:55 +0200

    added truncation to the task heading

--- src/components/CardContent.jsx
+++ src/components/CardContent.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const CardContent = ({ task }) => {
   return (
     <>
-      <h3>{task.task}</h3>
+      <h3>
+        {task.task.length <= 30 ? task.task : `${task.task.slice(0, 30)}...`}
+      </h3>
       <p>{task.date}</p>
     </>
   );
