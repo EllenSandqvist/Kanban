98a3eb6669adada23ca96d9c40b6dc5e68d6ee4c
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-04-03 12:18:55 +0200

    added arrows for moving tasks between columns

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import CardContent from "./CardContent";
 import TaskModal from "./TaskModal";
 import { MdOutlineOpenInFull } from "react-icons/md";
+import doingArrowRight from "../assets/doingToRight.png";
+import doingArrowLeft from "../assets/doingToLeft.png";
+import todoArrow from "../assets/todo.png";
+import doneArrow from "../assets/done.png";
+import TaskContext from "../context/TaskContext";
 
 const Card = ({ task }) => {
+  const { handleMoveTask } = useContext(TaskContext);
+
   const [modalShown, setModalShown] = useState(false);
   const [selectedTask, setSelectedTask] = useState({});
 
@@ -25,6 +32,49 @@ const Card = ({ task }) => {
         >
           <MdOutlineOpenInFull />
         </button>
+
+        <img
+          role="button"
+          title="Move task to Doing"
+          className="Doing"
+          src={
+            task.columnName === "Todo"
+              ? doingArrowRight
+              : task.columnName === "Done"
+              ? doingArrowLeft
+              : ""
+          }
+          alt={
+            task.columnName === "Todo"
+              ? "Doing Arrow pointing Right"
+              : task.columnName === "Done"
+              ? "Doing Arrow pointing Left"
+              : ""
+          }
+          onClick={(e) => handleMoveTask(e, task)}
+        />
+
+        {task.columnName === "Doing" && (
+          <div>
+            <img
+              role="button"
+              src={todoArrow}
+              alt="todo arrow"
+              className="Todo"
+              title="Move task to Todo"
+              onClick={(e) => handleMoveTask(e, task)}
+            />
+
+            <img
+              role="button"
+              src={doneArrow}
+              alt="done arrow"
+              className="Done"
+              title="Move task to Done"
+              onClick={(e) => handleMoveTask(e, task)}
+            />
+          </div>
+        )}
       </div>
       {modalShown && (
         <TaskModal
