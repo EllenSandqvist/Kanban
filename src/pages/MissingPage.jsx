63492054334ca4b4b3957fb8120623d8cbd236f7
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-04-04 13:52:47 +0200

    Created and styled the missingPage

--- src/pages/MissingPage.jsx
+++ src/pages/MissingPage.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Link } from "react-router-dom";
+
+//import of images/icons
+import missing from "../assets/missing404.svg";
 
 const MissingPage = () => {
-  return <div>MissingPage</div>;
+  return (
+    <main className="MissingPage">
+      <div className="MissingPage-div">
+        <img src={missing} alt="404 page not found" />
+        <p className="MissingPage-text">Whoops! It looks like you're lost!</p>
+      </div>
+      <button className="MissingPage-btn" type="button">
+        <Link className="MissingPage-link" to="/">
+          Back to Home
+        </Link>
+      </button>
+    </main>
+  );
 };
 
 export default MissingPage;
