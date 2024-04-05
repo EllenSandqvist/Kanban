23f982fd98947cc0c80a79ffedac179e8bcd4ac2
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-04-05 10:02:54 +0200

    Divided the code in Card.jsx so that Card.jsx handles the logic while CardContent renders out the elements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,12 +5,6 @@ import TaskContext from "../context/TaskContext";
 import CardContent from "./CardContent";
 import TaskModal from "./TaskModal";
 
-//import of images/icons
-import doingArrowLeft from "../assets/doingToLeft.png";
-import doingArrowRight from "../assets/doingToRight.png";
-import doneArrow from "../assets/done.png";
-import todoArrow from "../assets/todo.png";
-
 const Card = ({ task }) => {
   const {
     todoTasks,
@@ -64,67 +58,16 @@ const Card = ({ task }) => {
     }
   };
 
-  //img variables with different values depending on column
-  const arrowSrc =
-    task.columnName === "Todo"
-      ? doingArrowRight
-      : task.columnName === "Done"
-      ? doingArrowLeft
-      : "";
-  const arrowAlt =
-    task.columnName === "Todo"
-      ? "Doing arrow pointing right"
-      : task.columnName === "Done"
-      ? "Doing arrow pointing left"
-      : "";
-
   return (
     <>
-      <div
-        className="Card"
-        onClick={!modalShown ? () => toggleModal(task) : null}
-      >
-        <div className="CardContent">
-          <CardContent task={task} />
-        </div>
-
-        <img
-          role="button"
-          title="Move task to Doing"
-          className="Doing"
-          src={arrowSrc}
-          alt={arrowAlt}
-          style={{
-            alignSelf: task.columnName === "Todo" ? "end" : "start",
-          }}
-          onClick={(e) => handleMoveTask(e, task)}
-        />
-
-        {task.columnName === "Doing" && (
-          <div className="arrow-div">
-            <img
-              role="button"
-              src={todoArrow}
-              alt="todo arrow"
-              className="Todo"
-              title="Move task to Todo"
-              onClick={(e) => handleMoveTask(e, task)}
-            />
-
-            <img
-              role="button"
-              src={doneArrow}
-              alt="done arrow"
-              className="Done"
-              title="Move task to Done"
-              onClick={(e) => handleMoveTask(e, task)}
-            />
-          </div>
-        )}
-      </div>
+      <CardContent
+        task={task}
+        handleMoveTask={handleMoveTask}
+        modalShown={modalShown}
+        toggleModal={toggleModal}
+      />
       {modalShown && (
         <TaskModal
-          modalShown={modalShown}
           setModalShown={setModalShown}
           selectedTask={selectedTask}
           setSelectedTask={setSelectedTask}
--- src/components/CardContent.jsx
+++ src/components/CardContent.jsx
@@ -1,11 +1,70 @@
-const CardContent = ({ task }) => {
+//import of images/icons
+import doingArrowLeft from "../assets/doingToLeft.png";
+import doingArrowRight from "../assets/doingToRight.png";
+import doneArrow from "../assets/done.png";
+import todoArrow from "../assets/todo.png";
+
+const CardContent = ({ handleMoveTask, modalShown, task, toggleModal }) => {
+  //img variables with different values depending on column
+  const arrowSrc =
+    task.columnName === "Todo"
+      ? doingArrowRight
+      : task.columnName === "Done"
+      ? doingArrowLeft
+      : "";
+  const arrowAlt =
+    task.columnName === "Todo"
+      ? "Doing arrow pointing right"
+      : task.columnName === "Done"
+      ? "Doing arrow pointing left"
+      : "";
+
   return (
-    <>
-      <h3>
-        {task.task.length <= 30 ? task.task : `${task.task.slice(0, 30)}...`}
-      </h3>
-      <p>{task.date}</p>
-    </>
+    <div className="CardContent">
+      <div
+        className="CardContent-text"
+        onClick={!modalShown ? () => toggleModal(task) : null}
+      >
+        <h3>
+          {task.task.length <= 30 ? task.task : `${task.task.slice(0, 30)}...`}
+        </h3>
+        <p>{task.date}</p>
+      </div>
+
+      <img
+        role="button"
+        title="Move task to Doing"
+        className="Doing"
+        src={arrowSrc}
+        alt={arrowAlt}
+        style={{
+          alignSelf: task.columnName === "Todo" ? "end" : "start",
+        }}
+        onClick={(e) => handleMoveTask(e, task)}
+      />
+
+      {task.columnName === "Doing" && (
+        <div className="arrow-div">
+          <img
+            role="button"
+            src={todoArrow}
+            alt="todo arrow"
+            className="Todo"
+            title="Move task to Todo"
+            onClick={(e) => handleMoveTask(e, task)}
+          />
+
+          <img
+            role="button"
+            src={doneArrow}
+            alt="done arrow"
+            className="Done"
+            title="Move task to Done"
+            onClick={(e) => handleMoveTask(e, task)}
+          />
+        </div>
+      )}
+    </div>
   );
 };
 
